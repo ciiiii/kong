57424714deb82ed5569c84b1d3737fe0a910b1fc
caiyisheng <caiyisheng@corp-ci.com>
2019-10-23 17:55:02 +0800

    test build on root path

--- kong.sh
+++ kong.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 OPENRESTY_VERSION=1.15.8.1
-LUAROCKS_VERSION=3.0.4
+LUAROCKS_VERSION=3.1.3
+OPENSSL_VERSION=1.1.1b
 KONG_VERSION=1.3.0
 
 echo "*************************************************************************"
@@ -17,35 +18,30 @@ echo "*************************************************************************"
 echo "*************************************************************************"
 echo "Building openresty"
 echo "*************************************************************************"
-if [ -d "openresty-build-tools" ]; then
-    echo "already clone"
-    rm -rf openresty-build-tools/work rm -rf openresty-build-tools/buildroot
-else
-    git clone https://github.com/Kong/openresty-build-tools.git
-fi
+
+sudo su
+
+cd /
+
+git clone https://github.com/Kong/openresty-build-tools.git
+
 cd openresty-build-tools
 ./kong-ngx-build \
     -p buildroot \
     --openresty $OPENRESTY_VERSION \
-    --openssl 1.1.1b \
+    --openssl $OPENSSL_VERSION \
     --luarocks $LUAROCKS_VERSION \
     --force
-# echo "export PATH=$(pwd)/buildroot/luarocks/bin:\$PATH" >>~/.bashrc
-# echo "export PATH=$(pwd)/buildroot/openssl/bin:\$PATH" >>~/.bashrc
-# echo "export PATH=$(pwd)/buildroot/openresty/bin:\$PATH" >>~/.bashrc
-# echo "export PATH=$(pwd)/buildroot/openresty/nginx/sbin:\$PATH" >>~/.bashrc
-sudo ln -s "$(pwd)/buildroot/luarocks/bin/luarocks" /usr/bin/luarocks
-sudo ln -s "$(pwd)/buildroot/openresty/nginx/sbin/nginx" /usr/bin/nginx
-sudo ln -s "$(pwd)/buildroot/openresty/bin/resty" /usr/bin/resty
-sudo ln -s "$(pwd)/buildroot/openresty/bin/openresty" /usr/bin/openresty
-sudo mv "$(pwd)/buildroot/openresty/bin/resty" /usr/bin
 
-# source ~/.bashrc
-# eval "$(cat ~/.bashrc | tail -n +10)"
+ln -s "$(pwd)/buildroot/luarocks/bin/luarocks" /usr/bin/luarocks
+ln -s "$(pwd)/buildroot/openresty/nginx/sbin/nginx" /usr/bin/nginx
+ln -s "$(pwd)/buildroot/openresty/bin/resty" /usr/bin/resty
+ln -s "$(pwd)/buildroot/openresty/bin/openresty" /usr/bin/openresty
+mv "$(pwd)/buildroot/openresty/bin/resty" /usr/bin
 luarocks --version
 openresty -v
 nginx -v
-cd
+cd /
 
 echo "*************************************************************************"
 echo "Installing kong dependencies"
@@ -53,32 +49,53 @@ echo "*************************************************************************"
 wget -O "kong-${KONG_VERSION}.tar.gz" "https://github.com/Kong/kong/archive/${KONG_VERSION}.tar.gz"
 tar -xvf "kong-${KONG_VERSION}.tar.gz"
 cd "kong-${KONG_VERSION}"
-# luarocks install pgmoon 1.10.0
-# luarocks install lua-pack 1.0.5
 make install
 
-sudo cp bin/kong /usr/bin/kong
-sudo chmod 777 /usr/bin/kong
-# echo "export PATH=$(pwd)/bin:\$PATH" >>~/.bashrc
-# source ~/.bashrc
-# eval "$(cat ~/.bashrc | tail -n +10)"
+ln -s "$(pwd)/bin/kong" /usr/bin/kong
 eval "$(luarocks path)"
-kong version
+kong version -vv
+cd /
 
 echo "*************************************************************************"
 echo "Configuring kong"
 echo "*************************************************************************"
-if [ -d "/etc/kong" ]; then
-    echo "/etc/kong exist"
-else
-    sudo mkdir -p /etc/kong
-fi
-wget https://gist.githubusercontent.com/ciiiii/4f5fa80e02f820d7e7490bc9854da59f/raw/10aee35f4268c61cb0c52e9d0f62115f4b820eba/kong.conf
-sudo cp kong.conf /etc/kong/kong.conf
-if [ -d "/kong" ]; then
-    echo "/kong exist"
-else
-    sudo mkdir /kong
-fi
-sudo chown -R "travis:travis" /kong
-kong start
+
+sudo mkdir -p /etc/kong
+echo "prefix = /kong/
+pg_host = 192.168.0.36
+pg_password = \"kong\"
+proxy_listen = 0.0.0.0:80, 0.0.0.0:443 ssl
+" >/etc/kong/kong.conf
+
+mkdir /kong
+
+echo "*************************************************************************"
+echo "Configuring systemd"
+echo "*************************************************************************"
+
+export LUAROCKS_PREFIX=$(pwd)/openresty-build-tools/buildroot/luarocks
+echo "LUA_PATH=\"${LUAROCKS_PREFIX}/share/lua/5.1/?.lua;;${LUAROCKS_PREFIX}/share/lua/5.1/?/init.lua;/kong-plugin/?.lua;/kong-plugin/?/init.lua\"" >/etc/systemd/system/kong.env
+
+echo "[Unit]
+Description=kong
+After=network.target
+[Service]
+# Restart=always
+User=root
+Type=simple
+EnvironmentFile=/etc/systemd/system/kong.env
+ExecStart=/usr/bin/kong start -v
+ExecReload=/usr/bin/kong reload -v
+ExecStop=/usr/bin/kong stop -v
+PIDFile=/kong/pids/nginx.pid
+
+[Install]
+WantedBy=multi-user.target" >/etc/systemd/system/kong.service
+
+systemctl daemon-reload
+systemctl enable kong.service
+systemctl start kong.service
+sleep 10
+
+res="$(curl localhost 2>/dev/null)"
+echo $res
