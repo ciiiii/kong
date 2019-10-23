4345e63219043632b05dc285743d52ce032942ad
caiyisheng <caiyisheng@corp-ci.com>
2019-10-23 18:16:24 +0800

    use travis postgres service

--- kong.sh
+++ kong.sh
@@ -6,51 +6,6 @@ OPENRESTY_VERSION=1.15.8.1
 LUAROCKS_VERSION=3.1.3
 OPENSSL_VERSION=1.1.1b
 KONG_VERSION=1.3.0
-POSTGRES_VERSION=9.6
-
-echo "*************************************************************************"
-echo "Configuring postgres"
-echo "*************************************************************************"
-
-dpkg -f noninteractive --list postgresql-$POSTGRES_VERSION >/dev/null 2>&1
-sudo -E apt-get install postgresql-$POSTGRES_VERSION -qq
-
-sudo sed -i "s/#listen_address.*/listen_addresses '*'/" /etc/postgresql/$POSTGRES_VERSION/main/postgresql.conf
-
-sudo bash -c "cat > /etc/postgresql/$POSTGRES_VERSION/main/pg_hba.conf" <<EOL
-local   all             all                                     trust
-host    all             all             127.0.0.1/32            trust
-host    all             all             ::1/128                 trust
-host    all             all             0.0.0.0/0               trust
-EOL
-
-    sudo systemctl -q enable postgresql
-    sudo /etc/init.d/postgresql restart
-
-    # Create Postgres role and databases
-    psql -U postgres <<EOF
-\x
-CREATE ROLE kong;
-ALTER ROLE kong WITH login;
-CREATE DATABASE kong OWNER kong;
-CREATE DATABASE kong_tests OWNER kong;
-EOF
-
-    psql -d kong -U postgres <<EOF
-\x
-DROP SCHEMA IF EXISTS public CASCADE;
-CREATE SCHEMA IF NOT EXISTS public AUTHORIZATION kong;
-GRANT ALL ON SCHEMA public TO kong;
-EOF
-
-    psql -d kong_tests -U postgres <<EOF
-\x
-DROP SCHEMA IF EXISTS public CASCADE;
-CREATE SCHEMA IF NOT EXISTS public AUTHORIZATION kong;
-GRANT ALL ON SCHEMA public TO kong;
-EOF
-
-fi
 
 echo "*************************************************************************"
 echo "Building openresty"
@@ -97,6 +52,7 @@ echo "*************************************************************************"
 
 sudo mkdir -p /etc/kong
 echo "prefix = /kong/
+pg_password = \"kong\"
 proxy_listen = 0.0.0.0:80, 0.0.0.0:443 ssl
 " >/etc/kong/kong.conf
 
